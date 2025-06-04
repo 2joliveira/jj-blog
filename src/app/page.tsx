46d565d9f31f298420d7dc2940cb22df072e02f3
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-06-04 15:45:48 -0300

    config metadata to improve SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import { LandingPage } from "@/templates/landing-page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Site.Set",
+  description: "Landing Page Blog",
+  robots: "index, follow",
+  openGraph: {
+    title: "Site.Set",
+    description: "Landing Page Blog",
+    url: "https://jj-blog-eta.vercel.app/og-image.jpg",
+    siteName: "Site.Set",
+    locale: "pt-BR",
+    type: "website",
+    images: [
+      {
+        url: "https://jj-blog-eta.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Site.Set",
+      },
+    ],
+  },
+};
 
 export default function HomePage() {
-  return (
-    <>
-      <LandingPage />
-    </>
-  );
+  return <LandingPage />;
 }
